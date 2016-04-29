be5d6ce37f27b6f50dab0aa56d9c535c75bfc7ea
mclarke47 <matthewclarke47@gmail.com>
2016-04-29 16:57:45 +0100

    remove unused code

--- src/main/java/com/ft/membership/logging/LoggingTerminal.java
+++ src/main/java/com/ft/membership/logging/LoggingTerminal.java
@@ -1,6 +1,6 @@
 package com.ft.membership.logging;
 
 public interface LoggingTerminal {
-    public void log();
-    public void log(Object actorOrLogger);
+    void log();
+    void log(Object actorOrLogger);
 }
